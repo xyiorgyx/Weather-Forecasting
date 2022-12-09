91977b3998fc40f0a15dfe99794841b84323084e
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-12-09 17:21:48 -0500

    CSS and Java
    Added minimal code for the front page and javascript code.  a button was also added.

---
+++ assets/javascript.js
@@ -0,0 +1,13 @@
+const requestUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={key};
+const requestGeoUrl = api.openweathermap.org/geo/1.0/direct?q={location}&limit=5&appid={key};
+apiKey = '';
+var location = '';
+
+
+function displayWeather (){
+    for (i=0; i > data.length; i++)
+    const dayDiv = document.createElement('div');
+
+
+
+}
