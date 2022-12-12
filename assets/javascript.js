59e7ef535b189c714eddf019acdfa63b2f46af88
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-12-12 10:30:03 -0500

    javascript
    javascript code added to fetch lat and lon  coordinates

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,34 @@
-const requestUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={key};
-const requestGeoUrl = api.openweathermap.org/geo/1.0/direct?q={location}&limit=5&appid={key};
-apiKey = '';
+//var inputBox =  document.
+
+var userLocation = document.inputbox.textContent
+
+// var requestWeather = 
+var requestGeoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + 'charlotte' + '&limit=1&appid=b2888db4f0baa774a62c34dc4c426cad';
+
 var location = '';
 
 
-function displayWeather (){
-    
-    for (i=0; i > data.length; i++)
-    const dayDiv = document.createElement('div');
+function fetchCoordinates (){
 
+fetch(requestGeoUrl, {
+  method: 'GET', 
+  credentials: 'same-origin',
+  redirect: 'follow', 
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
 
 }
+requestGeoUrl();
+// function displayWeather (){
+    
+//     for (i=0; i > data.length; i++)
+//     const dayDiv = document.createElement('div');
+// }
+
+inputButton.addEventListener('click', fetchcoordinates);
