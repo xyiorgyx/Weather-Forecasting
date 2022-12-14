74c8e2b3091d9ec0abe8e9220cc5f39ef66aad26
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-12-14 16:12:06 -0500

    java
    script now returns information on the weather

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,12 +1,6 @@
 
 var requestGeoUrl = 'api.openweathermap.org/geo/1.0/direct?q=oceanside&limit=5&appid=22546ad54811a5933d3ccfa20cc45068';
 
-var requestWeather = `api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=22546ad54811a5933d3ccfa20cc45068`;
-
-var location = '';
-
-
-
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=oceanside&limit=5&appid=22546ad54811a5933d3ccfa20cc45068", {
   method: 'GET',
   credentials: 'same-origin',
@@ -20,26 +14,24 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=oceanside&limit=5&appid=22
     console.log(data[0].lat)
     console.log(data[0].lon)
 
-    var lat = data[0].lat
-    var lon = data[0].lon
+    var lat = data[0].lat;
+    var lon = data[0].lon;
+
+    displayWeather(lat, lon);
   });
 
-
-
-
-function fetchWeather() {
-  fetch(requestWeather, {
-    method: 'GET',
-    credentials: 'same-origin',
-    redirect: 'follow',
+function displayWeather(lat, lon){
+fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=22546ad54811a5933d3ccfa20cc45068`, {
+  method: 'GET',
+  credentials: 'same-origin',
+  redirect: 'follow',
+})
+  .then(function (response) {
+    return response.json();
   })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-
-
+  .then(function (data) {
+    console.log(data);
+  });
 }
+
 // inputButton.addEventListener('click', fetchCoordinates);
