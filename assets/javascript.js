88ace7829562e1743643f63bf5b0d8057365e470
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-12-20 15:35:06 -0500

    java css
    a reset.css file and some other changes were added for more predictability.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,6 @@
 
 var requestGeoUrl = 'api.openweathermap.org/geo/1.0/direct?q=oceanside&limit=5&appid=22546ad54811a5933d3ccfa20cc45068';
-
+// the following function sends an api request for the coordinates of a given city
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=oceanside&limit=5&appid=22546ad54811a5933d3ccfa20cc45068", {
   method: 'GET',
   credentials: 'same-origin',
@@ -16,12 +16,13 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=oceanside&limit=5&appid=22
 
     var lat = data[0].lat;
     var lon = data[0].lon;
-
-    displayWeather(lat, lon);
+    var cityName = data[0].name;
+    var state = data[0].state;
+    aquireWeatherData(lat, lon, cityName, state);
   });
-
-function displayWeather(lat, lon){
-fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=22546ad54811a5933d3ccfa20cc45068`, {
+// the next function takes the lat and lon of a given city and requests weather information in that area
+function aquireWeatherData(lat, lon, cityName, state){
+fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=22546ad54811a5933d3ccfa20cc45068`, {
   method: 'GET',
   credentials: 'same-origin',
   redirect: 'follow',
@@ -31,7 +32,23 @@ fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appi
   })
   .then(function (data) {
     console.log(data);
+    
+    displayWeather(data, cityName, state)
   });
+ 
 }
+// we now take the weather information that we requested and append it into our html file.
+function displayWeather(data, cityName, state){
+  const curWeatherBox = document.querySelector('#weatherSection');
+  const title = document.createElement('h1');
+  title.innerHTML= cityName + ', '+ state;
+  curWeatherBox.append(title);
+  var currentTemp = data.current.temp
+  var windSpeed = data.current.wind_speed;
+  var humidity = current.humidity;
+  var weatherIcon = current.weather[0].icon;
 
+
+}
 // inputButton.addEventListener('click', fetchCoordinates);
+
