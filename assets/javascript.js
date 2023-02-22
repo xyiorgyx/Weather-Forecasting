e6a74bf9a81adb6ea320e11ac58c19ac824deb26
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-02-21 22:28:04 -0500

    Javascript
    Styling and javascript code getting an overhaul

--- assets/javascript.js
+++ assets/javascript.js
@@ -38,7 +38,7 @@ function aquireWeatherData(lat, lon, cityName, state) {
 }
 // we now take the weather information that we requested and append it into our html file.
 function displayWeather(data, cityName, state) {
-  displayweeksWeather(data)
+  displayweeksWeather(data.daily);
   const curWeatherBox = document.querySelector('#weatherSection');
   const title = document.createElement('h1');
   title.innerHTML = cityName + ', ' + state;
@@ -65,38 +65,59 @@ function displayWeather(data, cityName, state) {
   
 }
 
-function displayweeksWeather(data) {
+// function displayweeksWeather(data) {
   
-  for (let i = 0; i < 5; i++) {
-    // var currentTemp = data.daily[0].
-    // var windSpeed = data.current.wind_speed;
-    // var humidity = data.current.humidity;
-    const weekWeatherBox = document.querySelector('#weeksWeather');
-    const dayDiv = document.createElement('div');
-    weekWeatherBox.append(dayDiv);
-    // const dateHeading = document.createElement('h2');
-    // var oldDate = data.dt
-    // var convertedDate = new Date(dateUTC * 1000)
-    // dateHeading.innerHTML =
-    // dayDiv.append(dateHeading) 
-    var weatherIcon = document.createElement('img')
-    var icon = data.daily[0].weather[0].icon
-    var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-    dayDiv.append(weatherIcon);
-    weatherIcon.setAttribute('src', iconurl);
-    const list = document.createElement('ul');
-    var listItem = document.createElement('li');
-    var listItem2 = document.createElement('li');
-    var listItem3 = document.createElement('li');
-    list.append(listItem)
-    list.append(listItem2)
-    list.append(listItem3)
-    var currentTemp = data.daily[0].temp.day;
-    listItem.append('Current Temperature: ' + currentTemp + " F°");
-    var windSpeed = data.daily[0].wind_speed;
-    listItem2.append('Current Wind Speed: ' + windSpeed + ' mph');
-    var humidity = data.daily[0].humidity;
-    listItem3.append('Current Humidity ' + humidity + " %");
-    console.log(list)
-  }
+//   for (let i = 0; i < 5; i++) {
+  
+//     const weekWeatherBox = document.querySelector('#weeksWeather');
+//     const dayDiv = document.createElement('div');
+//     weekWeatherBox.append(dayDiv);
+
+//     var weatherIcon = document.createElement('img')
+//     var icon = data.daily[0].weather[0].icon
+//     var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+//     dayDiv.append(weatherIcon);
+//     weatherIcon.setAttribute('src', iconurl);
+//     const list = document.createElement('ul');
+//     var listItem = document.createElement('li');
+//     var listItem2 = document.createElement('li');
+//     var listItem3 = document.createElement('li');
+//     list.append(listItem)
+//     list.append(listItem2)
+//     list.append(listItem3)
+//     var currentTemp = data.daily[0].temp.day;
+//     listItem.append('Current Temperature: ' + currentTemp + " F°");
+//     var windSpeed = data.daily[0].wind_speed;
+//     listItem2.append('Current Wind Speed: ' + windSpeed + ' mph');
+//     var humidity = data.daily[0].humidity;
+//     listItem3.append('Current Humidity ' + humidity + " %");
+//     console.log(list)
+//   }
+// }
+
+
+function displayweeksWeather(data){
+var weatherHtml = '';
+
+for(let i=0; i<5; i++){
+  var current = data[i];
+
+  weatherHtml += `
+  <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${current.dt}</h5>
+          <ul>
+            <li>Temp: ${current.temp.day}degrees</li>
+            <li>Wind Speed: ${current.wind_speed}knots</li>
+            <li>Humidty: ${current.humidity}%</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+$("#weeksWeather").html(weatherHtml)
+
 }
